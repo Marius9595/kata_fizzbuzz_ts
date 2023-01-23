fb6d36ee8abbc69168fcc3e7c192261260a2eb0b
Mario Pinto <mario@leanmind.es>
2023-01-23 20:03:49 +0000

    init

---
+++ src/tests/fizzbuzz.test.ts
@@ -0,0 +1,18 @@
+import { fizbuzz } from '../core/fizzbuzz';
+
+/*
+Examples to test (in order)
+
+ - 1 -> 1
+ - 3 -> "buzz"
+ - 5 -> "fizz"
+ - 15 -> "fizzbuzz"
+ - any number divisible by 3 -> "buzz"
+ - any number divisible by 5 -> "fizz"
+ - any number divisible by 3 and 5 -> "fizzbuzz"
+ - the rest of numbers -> number
+ */
+
+describe('Fizzbuzz should', () => {
+	it('');
+});
